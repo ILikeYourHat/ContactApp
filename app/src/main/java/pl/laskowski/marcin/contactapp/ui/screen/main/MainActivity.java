9d892e185c2bb44f7c9b648fa71b3dcfb1072683
Marcin Laskowski <marcin.laskowski@senfino.com>
2017-12-09 16:03:23 +0100

    Added first activity

---
+++ app/src/main/java/pl/laskowski/marcin/contactapp/ui/screen/main/MainActivity.java
@@ -0,0 +1,22 @@
+package pl.laskowski.marcin.contactapp.ui.screen.main;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import pl.laskowski.marcin.contactapp.R;
+
+/**
+ * Created by Marcin Laskowski.
+ * Senfino 2017
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
